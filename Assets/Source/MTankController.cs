6cf3caee628184b427ba02e1f3404d416aa7516c
Juraj Fiala <DoctorJellyface@users.noreply.github.com>
2014-11-02 07:02:45 +0100

    Deleted all the useless MD comments from MTankController, references #20.

--- Assets/Source/MTankController.cs
+++ Assets/Source/MTankController.cs
@@ -23,29 +23,14 @@ using UnityEngine;
 [AddComponentMenu("Mixed/Tank Controller")]
 //[RequireComponent (typeof (Rigidbody))]
 
-/// <summary>
-/// (Shared) Tank controller.
-/// </summary>
 public class MTankController : MonoBehaviour {
 
-	/// <summary>
-	/// The rotation variable.
-	/// </summary>
 	public static float rotVar = 2f;
-	
-	/// <summary>
-	/// The rotations.
-	/// </summary>
+
 	public static Vector3[] rotations = new Vector3[4] {new Vector3 (0,0,0), new Vector3 (0,90,0), new Vector3 (0,180,0), new Vector3 (0,270,0)};
-	
-	/// <summary>
-	/// The rotations in Quaternion.
-	/// </summary>
+
 	public static Quaternion[] rotationsQ = new Quaternion[4];
-	
-	/// <summary>
-	/// Initializes the <see cref="M_TankController"/> class.
-	/// </summary>
+
 	static MTankController () {
 		
 		Debug.Log ("Run Constructor.");
@@ -54,14 +39,7 @@ public class MTankController : MonoBehaviour {
 		}
 		
 	}
-	
-	/// <summary>
-	/// Gets the new rotation.
-	/// </summary>
-	/// <returns>The new rotation.</returns>
-	/// <param name="horizontal">Horizontal axis.</param>
-	/// <param name="vertical">Vertical axis.</param>
-	/// <param name="last">Last rotation.</param>
+
 	static Quaternion GetNewRotation (int horizontal, int vertical, Quaternion last) {
 	
 			if (horizontal == 1 && vertical == 0) {
@@ -90,19 +68,9 @@ public class MTankController : MonoBehaviour {
 			}
 			return last;
 		}
-	
-	/// <summary>
-	/// Gets the new rotation phase.
-	/// </summary>
-	/// <returns><c>true</c>, if new rotation phase was gotten, <c>false</c> otherwise.</returns>
-	/// <param name="tankTransform">Tank transform.</param>
-	/// <param name="fromRotation">From rotation.</param>
-	/// <param name="toRotation">To rotation.</param>
-	/// <param name="speed">Speed.</param>
-	/// <param name="lastSuccTo">Last successful to rotation.</param>
-	/// <param name="lastTo">Last to rotation.</param>
+
 	static bool GetNewRotationPhase (Transform tankTransform, Quaternion fromRotation, Quaternion toRotation, float speed, ref Quaternion lastSuccTo, ref Quaternion lastTo) {
-		//MAYBEUPGRADE Rotate with force
+		//TODO Rotate with force
 		float timing = Quaternion.Angle (fromRotation, toRotation) / (speed * 120f);
 		float rate = 1f / timing;
 		float t = 0f;
@@ -122,14 +90,7 @@ public class MTankController : MonoBehaviour {
 	
 	}
 	
-	/// <summary>
-	/// Gets the new rotation phase (only for turrets).
-	/// </summary>
-	/// <returns><c>true</c>, if new rotation phase was gotten, <c>false</c> otherwise.</returns>
-	/// <param name="tankTransform">Tank transform.</param>
-	/// <param name="fromRotation">From rotation.</param>
-	/// <param name="toRotation">To rotation.</param>
-	/// <param name="speed">Speed.</param>
+	//Turrets
 	static bool GetNewRotationPhase (Transform tankTransform, Quaternion fromRotation, Quaternion toRotation, float speed) {
 		
 		float timing = Quaternion.Angle (fromRotation, toRotation) / (speed * 2000f);
@@ -142,15 +103,7 @@ public class MTankController : MonoBehaviour {
 		return (Quaternion.Angle (fromRotation, toRotation) < rotVar);
 		
 	}
-	
-	/// <summary>
-	/// Adds the force.
-	/// </summary>
-	/// <returns>The force.</returns>
-	/// <param name="direction">Direction.</param>
-	/// <param name="velocity">Velocity.</param>
-	/// <param name="speed">Speed.</param>
-	/// <param name="maximumVelocityChange">Maximum velocity change.</param>
+
 	static public Vector3 AddForce (Vector3 direction, Vector3 velocity, float speed, float maximumVelocityChange) {
 	
 		Vector3 targetVelocity = direction;
@@ -167,48 +120,19 @@ public class MTankController : MonoBehaviour {
 		//rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
 	
 	}
-	
-	/// <summary>
-	/// Calculates the middle.
-	/// </summary>
-	/// <returns>The middle.</returns>
-	/// <param name="rot1">Rotation 1.</param>
-	/// <param name="rot2">Rotation 2.</param>
+
 	static Quaternion CalculateMiddle (Quaternion rot1, Quaternion rot2) {
 		return Quaternion.Slerp (rot1,rot2,0.5f);	
 	}
-	
-	/// <summary>
-	/// Gets the horizontal axis.
-	/// </summary>
-	/// <returns>The horizontal axis.</returns>
-	/// <param name="a">"A" key.</param>
-	/// <param name="d">"D" key.</param>
+
 	static public int GetHorizontalAxis (bool a, bool d) {		
 		return System.Convert.ToInt32 (d) - System.Convert.ToInt32 (a);		
 	}
-	
-	/// <summary>
-	/// Gets the vertical axis.
-	/// </summary>
-	/// <returns>The vertical axis.</returns>
-	/// <param name="s">"S" key.</param>
-	/// <param name="w">"W" key.</param>
+
 	static public int GetVerticalAxis (bool s, bool w) {
 		return System.Convert.ToInt32 (w) - System.Convert.ToInt32 (s);		
 	}
-	
-	/// <summary>
-	/// Main move method.
-	/// </summary>
-	/// <param name="tank">Tank.</param>
-	/// <param name="h">The horizontal axis.</param>
-	/// <param name="v">The vertical axis.</param>
-	/// <param name="moveSpeed">Move speed.</param>
-	/// <param name="maxVChange">Max Velocity change.</param>
-	/// <param name="maxVChangeStop">Max stop velocity change.</param>
-	/// <param name="lastSuccRot">Last succ rot.</param>
-	/// <param name="lastRot">Last rot.</param>
+
 	static public void MoveMain (GameObject tank, int h, int v, float mSpeed, float rSpeed, float maxVChange, float maxVChangeStop, ref Quaternion lastSuccRot, ref Quaternion lastRot, ref bool isItMoving) {		
 		Quaternion newRot = GetNewRotation (h, v, lastRot);
 	
@@ -233,60 +157,22 @@ public class MTankController : MonoBehaviour {
 		Tank,
 		Turret
 	}
-	
-	/// <summary>
-	/// The type of the controller needed.
-	/// </summary>
+
 	public Type type;
-	
-	/// <summary>
-	/// The speed.
-	/// </summary>
 	public float moveSpeed = 5f;
-	
 	public float rotSpeed = 2.5f;
-	
-	/// <summary>
-	/// The max velocity change.
-	/// </summary>
 	public float maxVelocityChange = 10f;
-	
-	/// <summary>
-	/// The max stop velocity change.
-	/// </summary>
 	public float maxVelocityChangeStop = 2f;
-	
-	/// <summary>
-	/// The last successful rotation.
-	/// </summary>
 	public Quaternion lastSuccRotation;
-	
-	/// <summary>
-	/// The last rotation.
-	/// </summary>
 	public Quaternion lastRotation;
-	
-	/// <summary>
-	/// Is the tank applying force?
-	/// </summary>
 	public bool isMoving;
-	
-	/// <summary>
-	/// Move with the specified horizontal and vertical motion.
-	/// </summary>
-	/// <param name="h">The horizontal axis.</param>
-	/// <param name="v">The vertical axis.</param>
+
 	public void Move (int h, int v) {
 	
 		MTankController.MoveMain (gameObject, h, v, moveSpeed, rotSpeed, maxVelocityChange, maxVelocityChangeStop, ref lastSuccRotation, ref lastRotation, ref isMoving);
 	
 	}
-	
-	/// <summary>
-	/// Rotate the specified turret with the specified speed.
-	/// </summary>
-	/// <param name="turret">Turret.</param>
-	/// <param name="speed">Speed.</param>
+
 	public static Quaternion RotateToMouse (GameObject turret) {
 		
 		Plane playerPlane = new Plane (Vector3.up, turret.transform.position);
@@ -312,11 +198,7 @@ public class MTankController : MonoBehaviour {
 			return Quaternion.identity;
 	
 	}
-	/// <summary>
-	/// Local rotate method. called by the server and client
-	/// scripts
-	/// </summary>
-	/// <param name="toRotation">To rotation.</param>
+
 	public void Rotate (Quaternion toRotation) {
 	
 		MTankController.GetNewRotationPhase (transform, transform.rotation, toRotation, rotSpeed / 50f);
