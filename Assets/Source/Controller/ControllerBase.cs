16ff2cd30a26ad4e612e1ee92b6aab0b12094e83
Juraj Fiala <doctorjellyface@riseup.net>
2015-03-24 16:28:00 +0100

    Fixed last ugly part of ControllerBase.
    Replaced the calculations with hardcoded values, now it's much
    cleaner, quicker, and more human-readable.

--- Assets/Source/Controller/ControllerBase.cs
+++ Assets/Source/Controller/ControllerBase.cs
@@ -24,46 +24,35 @@ public class ControllerBase: MonoBehaviour	{
 
 	public static float rotVar = 2f;
 
-	public static Quaternion [] rotations = new Quaternion [] {
-		new Quaternion (0f, 0f, 0f, 1f),
-		new Quaternion (0f, 0.7071068f, 0f, 0.7071068f),
-		new Quaternion (0f, 1f, 0f, -4.371139e-08f),
-		new Quaternion (0f, 0.7071068f, 0f, -0.7071068f)
+	public static Quaternion [,] rotations = {
+		{
+			new Quaternion (0f, 0.9238796f, 0f, 0.3826834f),
+			new Quaternion (0f, 1f, 0f, -4.371139e-08f),
+			new Quaternion (0f, 0.9238796f, 0f, -0.3826835f)
+		},
+		{
+			new Quaternion (0f, 0.7071068f, 0f, 0.7071068f),
+			Quaternion.identity,
+			new Quaternion (0f, 0.7071068f, 0f, -0.7071068f)
+		},
+		{
+			new Quaternion (0f, 0.3826835f, 0f, 0.9238796f),
+			new Quaternion (0f, 0f, 0f, 1f),
+			new Quaternion (0f, -0.3826835f, 0f, 0.9238796f)
+		}
+
 	};
 	
 	public static Quaternion GetNewRotation (int horizontal, int vertical, Quaternion lastRot) {
 
-		if (horizontal == 1 && vertical == 0) {
-			return rotations [0];
-		}
-		if (horizontal == -1 && vertical == 0) {
-			return rotations [2];
-		}
-		if (vertical == -1 && horizontal == 0) {
-			return rotations [1];
-		}
-		if (vertical == 1 && horizontal == 0) {
-			return rotations [3];
-		}
-		if (horizontal == -1 && vertical == -1) {
-			return CalculateMiddle (rotations [2], rotations [1]);
-		}
-		if (horizontal == -1 && vertical == 1) {
-			return CalculateMiddle (rotations [2], rotations [3]);
-		}
-		if (horizontal == 1 && vertical == 1) {
-			return CalculateMiddle (rotations [0], rotations [3]);
-		}
-		if (horizontal == 1 && vertical == -1) {
-			return CalculateMiddle (rotations [0], rotations [1]);
-		}
+		// Are any keys pressed?
+		// If no, pass the last rotation
+		if (horizontal == 0 && vertical == 0)
+			return lastRot;
 
-		return lastRot;
+		// If yes, pass a rotation from the table
+		return rotations [horizontal + 1, vertical + 1];
 
-	}
-
-	public static Quaternion CalculateMiddle (Quaternion rot1, Quaternion rot2) {
-		return Quaternion.Slerp (rot1, rot2, 0.5f);
 	}
 
 	public static Vector3 AddForce (Vector3 direction, Vector3 velocity, float speed, float maximumVelocityChange) {
