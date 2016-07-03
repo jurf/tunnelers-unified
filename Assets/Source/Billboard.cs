8889b550221615cfcf3b6e3c71078ea4c7f7382c
Juraj Fiala <doctorjellyface@riseup.net>
2016-07-03 19:44:16 +0200

    Make the nick turn to the camera.

---
+++ Assets/Source/Billboard.cs
@@ -0,0 +1,34 @@
+﻿// vim:set ts=4 sw=4 sts=4 noet:
+//
+//  Billboard.cs is part of Tunnelers: Unified
+//  <https://github.com/VacuumGames/tunnelers-unified/>
+//
+//  Copyright (c) 2016 Juraj Fiala <doctorjellyface@riseup.net>
+//
+//  Tunnelers: Unified is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Tunnelers: Unified is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Tunnelers: Unified.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
+
+using UnityEngine;
+
+public class Billboard: MonoBehaviour {
+
+	void Update () {
+
+		var point = Camera.main.transform.position;
+		point.x = transform.position.x;
+
+		transform.LookAt(point);
+	}
+}
